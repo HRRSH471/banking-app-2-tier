d67fa7a1516a59cf414be350f7d033a9737e14d1
HRRSH471 <Hariomharish50@gmail.com>
2026-07-05 13:25:51 +0530

    Smoke Test

---
+++ smoke_test.py
@@ -0,0 +1,46 @@
+import boto3 ,os,sys
+import pymysql
+client=boto3.client('ec2',region_name='ap-northeast-2')\
+params={
+    os.path.basename(p["Name"]):p["Values"])
+    for p in client.get_parameters_by_path(
+        Path='/application/banking',
+        WithDecryption=True
+    )['Parameters']
+}
+Required=["DB_HOST","DB_NAME","DB_USER","DB_PASSWORD","DB_PORT"]
+missing=[k fir k in Required if k not in params]\
+
+
+for k in required:
+    print(f"{k}" :{"✅" if k in params else '❌')")
+    if k in params:
+        print(k, "✅")
+    else:
+        print(k, "❌")
+if missing:
+    print(f"Failed : {missing}")
+    sys.exit(1)
+# DB FIND banking_db and show tables
+
+try:
+    connection=pymysql.connect(
+        host=params["DB_HOST"],
+        user=params["DB_USER"],
+        password=params["DB_PASSWORD"],
+        database=params["DB_NAME"],
+        port=int(params["DB_PORT"]
+        connect_timeout=10
+    )
+
+    cur=connection.cursor()
+    cur.execute("SHOW TABLES")
+    tables=[row[0] for row in cur.fetchall()]
+    conenction.close()
+    print(f"{params['DB_NAME']})
+    print(f"Table : {tabels}")
+
+except pymysql.MySQLError as e:
+    print("DB ERROR ❌ : ", e)
+    sys.exit(1)
+print("✅ Smoke Test Done")
