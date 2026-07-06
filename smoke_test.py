8838840ec6ac8fb78c8d1f52d679fb01d99027f2
HRRSH471 <Hariomharish50@gmail.com>
2026-07-06 10:32:41 +0530

    Pipline

--- smoke_test.py
+++ smoke_test.py
@@ -1,46 +1,51 @@
-import boto3 ,os,sys
+import boto3
+import os
+import sys
 import pymysql
-client=boto3.client('ec2',region_name='ap-northeast-2')\
-params={
-    os.path.basename(p["Name"]):p["Values"])
+
+client = boto3.client('ssm', region_name='ap-northeast-2')
+
+params = {
+    os.path.basename(p["Name"]): p["Value"]
     for p in client.get_parameters_by_path(
         Path='/application/banking',
         WithDecryption=True
     )['Parameters']
 }
-Required=["DB_HOST","DB_NAME","DB_USER","DB_PASSWORD","DB_PORT"]
-missing=[k fir k in Required if k not in params]\
 
+Required = ["DB_HOST", "DB_NAME", "DB_USER", "DB_PASSWORD", "DB_PORT"]
 
-for k in required:
-    print(f"{k}" :{"✅" if k in params else '❌')")
-    if k in params:
-        print(k, "✅")
-    else:
-        print(k, "❌")
+missing = [k for k in Required if k not in params]
+
+for k in Required:
+    print(f"{k}: {'✅' if k in params else '❌'}")
+
 if missing:
     print(f"Failed : {missing}")
     sys.exit(1)
+
 # DB FIND banking_db and show tables
 
 try:
-    connection=pymysql.connect(
+    connection = pymysql.connect(
         host=params["DB_HOST"],
         user=params["DB_USER"],
         password=params["DB_PASSWORD"],
         database=params["DB_NAME"],
-        port=int(params["DB_PORT"]
+        port=int(params["DB_PORT"]),
         connect_timeout=10
     )
 
-    cur=connection.cursor()
+    cur = connection.cursor()
     cur.execute("SHOW TABLES")
-    tables=[row[0] for row in cur.fetchall()]
-    conenction.close()
-    print(f"{params['DB_NAME']})
-    print(f"Table : {tabels}")
+    tables = [row[0] for row in cur.fetchall()]
+    connection.close()
+
+    print(f"Database : {params['DB_NAME']}")
+    print(f"Tables : {tables}")
 
 except pymysql.MySQLError as e:
-    print("DB ERROR ❌ : ", e)
+    print("DB ERROR ❌ :", e)
     sys.exit(1)
+
 print("✅ Smoke Test Done")
